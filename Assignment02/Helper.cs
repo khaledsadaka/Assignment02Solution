f90819fb49526c7e2767272d4fbda44fe33140f2
AASTMT <AASTMT@DESKTOP-VRTFKDA>
2025-01-30 21:43:37 +0200

    Add project files.

---
+++ Assignment02/Helper.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    public class Helper
+    {
+        //1.
+        public static void CheckArray(int[] numbers, int queries)
+        {
+            for (int i = 0; i < queries; i++)
+            {
+                int num;
+                do
+                {
+                    Console.WriteLine("Please Enter X");
+                }
+                while (!int.TryParse(Console.ReadLine(), out num));
+
+                int count = 0;
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (num < numbers[j])
+                        count++;
+                }
+                Console.WriteLine($"There is {count} numbers is greater than X");
+            }
+        }
+
+
+
+        //3.
+        public static Queue ReverseQueue(Queue queue)
+        {
+            Stack stack = new Stack();
+
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+            while (stack.Count > 0)
+            {
+                queue.Enqueue(stack.Pop());
+            }
+
+            return queue;
+        }
+
+
+
+        //5. 
+
+        public static int[] RemoveDuplicate(int[] array)
+        {
+            HashSet<int> values = new HashSet<int>(array);
+
+            array = values.ToArray();
+            return array;
+        }
+
+        //6.
+
+        public static void RemoveAllOddNumbers(List<int> list)
+        {
+            list.RemoveAll(num => num % 2 != 0);
+        }
+
+
+
+        //7.
+
+        public static void CreateQueue()
+        {
+            Queue<object> queue = new Queue<object>();
+            queue.Enqueue(1);
+            queue.Enqueue("Apple");
+            queue.Enqueue(5.28);
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+        //9.
+
+        public static int[] FindIntersection(int[] array1, int[] array2)
+        {
+            Dictionary<int, int> countMap = new Dictionary<int, int>();
+
+
+            foreach (int num in array1)
+            {
+                if (countMap.ContainsKey(num))
+                {
+                    countMap[num]++;
+                }
+                else
+                {
+                    countMap[num] = 1;
+                }
+            }
+
+            List<int> intersection = new List<int>();
+
+
+            foreach (int num in array2)
+            {
+                if (countMap.ContainsKey(num) && countMap[num] > 0)
+                {
+                    intersection.Add(num);
+                    countMap[num]--;
+                }
+            }
+
+            return intersection.ToArray();
+        }
+
+
+
+        //10.
+
+        public static int[]? FindTarget(ArrayList arrlist, int target)
+        {
+            int count = arrlist.Count;
+            int[] arr = (int[])arrlist.ToArray(typeof(int));
+
+            for (int i = 0; i < count; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < count; j++)
+                {
+                    sum += arr[j];
+                    if (sum == target)
+                    {
+                        int[] result = new int[j - i + 1];
+                        Array.Copy(arr, i, result, 0, j - i + 1);
+                        return result;
+                    }
+                }
+            }
+            return null;
+        }
+        
+        
+    }
+}
---
+++ Assignment02/Program.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+
+namespace Assignment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.
+
+            /*
+            int[] nums = { 1, 10, 30, 44, 55 };
+            int queries = 2;
+            Helper.CheckArray(nums, queries);
+            */
+
+            /////////////////////////////////////////////////////////////////////
+
+            //2.
+
+            /*
+
+            int N = int.Parse(Console.ReadLine());
+
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+
+            bool isPalindrome = true;
+            for (int i = 0; i < N / 2; i++)
+            {
+                if (arr[i] != arr[N - i - 1])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            Console.WriteLine(isPalindrome ? "YES" : "NO");
+
+            */
+
+
+            /////////////////////////////////////////////////////////////////////
+
+
+
+            //3.
+
+            /*
+
+            Queue queue = new Queue();
+
+            for (int i = 1; i <= 15; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            Console.WriteLine("Queue Before Reverse");
+            foreach (int item in queue)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Write("\n");
+
+
+            queue = Helper.ReverseQueue(queue);
+            Console.WriteLine("Queue After Reverse");
+
+            foreach (int item in queue)
+            {
+                Console.Write(item + " ");
+            }
+
+
+            */
+
+            /////////////////////////////////////////////////////////////////////
+
+
+            //4.
+
+
+
+
+            /////////////////////////////////////////////////////////////////////
+
+
+
+            //5. 
+
+            /*
+
+            int[] nums = { 1, 2, 3, 2, 4, 4, 3, 2 };
+
+            nums = Helper.RemoveDuplicate(nums);
+            foreach (int item in nums)
+            {
+                Console.Write(item + " ");
+            }
+
+            */
+
+
+            /////////////////////////////////////////////////////////////////////
+
+
+            //6.
+
+            /*
+
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 9, 11, 13 };
+
+            Helper.RemoveAllOddNumbers(list);
+
+            Console.WriteLine(string.Join(" ", list));
+
+            */
+
+            /////////////////////////////////////////////////////////////////////
+
+
+
+            //7. 
+
+            /*
+            Helper.CreateQueue();
+            */
+
+
+            /////////////////////////////////////////////////////////////////////
+
+
+            //8.
+
+
+
+            /////////////////////////////////////////////////////////////////////
+
+
+            //9.
+
+            /*
+
+            int[] array1 = { 1, 2, 3, 4, 4 };
+            int[] array2 = { 10, 4, 4 };
+
+            int[] result = Helper.FindIntersection(array1, array2);
+
+            Console.WriteLine($"Intersection: [" + string.Join(", ", result) + "]");
+
+            */
+
+
+            /////////////////////////////////////////////////////////////////////
+
+
+
+            //10.
+
+            
+            ArrayList arrList = new ArrayList() { 1, 2, 3, 7, 5 };
+            int target = 12;
+            int[]? result = Helper.FindTarget(arrList, target);
+
+            foreach (int item in result)
+            {
+                Console.Write(item + " ");
+            }
+
+
+
+            /////////////////////////////////////////////////////////////////////
+
+
+
+            //11. 
+
+
+        }
+    }
+}
